e02ce35bf83bafac6a05fa94423d6afc73ab00b4
tracer-ics2023 <tracer@njuics.org>
2023-11-20 18:07:10 +0800

    >  compile NEMU 23050411 shun Linux awaken 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:07:10 up 10:37,  1 user,  load average: 0.58, 0.45, 0.36

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,15 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args){
+  int n;
+  if(args == NULL) n = 1;
+  else n = atoi(args);
+  cpu_exec(n);
+  return 0;
+
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -62,6 +71,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "step", cmd_si},
 
   /* TODO: Add more commands */
 

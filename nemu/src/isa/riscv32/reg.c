3d345100920bd8eb6b16790941d2261044173158
tracer-ics2023 <tracer@njuics.org>
2023-11-21 20:38:43 +0800

    >  compile NEMU 23050411 shun Linux awaken 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:38:43 up 23 min,  1 user,  load average: 0.30, 0.26, 0.33

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -15,7 +15,7 @@
 
 #include <isa.h>
 #include "local-include/reg.h"
-#define REGNUMBER sizeof(regs) / sizeof(regs[0])
+#define REGNUMBER ARRLEN(regs)
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",

a0e6112a863bbe2502d6f06048631bcebe8124d6
tracer-ics2023 <tracer@njuics.org>
2023-11-24 20:47:38 +0800

    >  compile NEMU 23050411 shun Linux awaken 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:38 up 14:43,  1 user,  load average: 0.53, 0.34, 0.24

--- nemu/src/isa/loongarch32r/init.c
+++ nemu/src/isa/loongarch32r/init.c
@@ -19,7 +19,8 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-  0x1c00000c,  // pcaddu12i $t0,0
+  //0x1c00000c,  // pcaddu12i $t0,0
+  0x14001801,  //ADD a1, #10, a2
   0x29804180,  // st.w $zero,$t0,16
   0x28804184,  // ld.w $a0,$t0,16
   0x002a0000,  // break 0 (used as nemu_trap)
